c29fed692bda79e665d5642179a03a79ed6de20e
gkljy <107994283+gkljy@users.noreply.github.com>
2023-11-23 15:47:26 +0800

    Update MainActivity.java3

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("123", "test1");
+        //其他人更改3
     }
     //其他人更改
     //其他人更改2
