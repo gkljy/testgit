5ccc166a66f4dd2274e0c7e5e1cf9e4a00088d9a
chengkangning <chengkangning@xiaomi.com>
2023-11-23 15:09:04 +0800

    Second commit
    Signed-off-by: chengkangning <chengkangning@xiaomi.com>

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("123", "test1");
     }
     //其他人更改
     //其他人更改2
